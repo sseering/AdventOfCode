c639e4f9e217781f6b84c5cca31e1847ec52f1ab
Stefan Seering <git-public@stefan.seeri.ng>
2024-12-03 23:55:14 +0100

    AoC 2024 03

--- StatusImg/img.py
+++ StatusImg/img.py
@@ -93,7 +93,7 @@ def main() -> None:
 
     progress.mark_progress(COLOR_SUCCESS, 2023, range(1, 5))
 
-    progress.mark_progress(COLOR_SUCCESS, 2024, range(1, 3))
+    progress.mark_progress(COLOR_SUCCESS, 2024, range(1, 4))
 
     with cairo.SVGSurface('StatusImg.svg', IMG_WIDTH, 435) as surface:
         cnt = cairo.Context(surface)
